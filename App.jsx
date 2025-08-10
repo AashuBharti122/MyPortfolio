057cae020ea4f681deb1bf4a4b7e0127c4063a80
AashuBharti122 <aashubharti122@gmail.com>
2025-08-10 12:42:16 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import About from "./Components/About/About"
+import Contact from "./Components/Contact/Contact"
+import Home from "./Components/Home/Home"
+import Nav from "./Components/Nav/Nav"
+import Pjt from "./Components/Projectss/Pjt"
+
+function App() {
+  return (
+  <>
+  <Nav />
+  <Home />
+  <About/>
+  <Pjt/>
+  <Contact/>
+  </>
+  )
+}
+
+export default App
